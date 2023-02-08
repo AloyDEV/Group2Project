1b4cfb7f1a4ee8d3195aecea28c4d4bd48c29e3f
Aloy <AloyOfRivia@protonmail.com>
2023-02-07 18:32:11 -0600

    Class & Function notes

--- Code/Source/GameStart.js
+++ Code/Source/GameStart.js
@@ -2,5 +2,26 @@
 //  https://en.wikipedia.org/wiki/Uno_(card_game)
 
 
-//TODO: Need to include proper error handling around EVERTHING
+//TODO: Need to include proper error handling around EVERYTHING
     //https://www.w3schools.com/js/js_errors.asp
+
+
+//Player Class
+
+
+//Player Hand Class
+
+
+//Deck Class
+
+
+//Shuffle Deck Function
+
+
+//Player Move Function
+
+
+//Current Player Function
+
+
+//Check Win Status Function
