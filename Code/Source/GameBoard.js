5aa78a3cdcb000ea2ebbcca8add60b0c773ce9b4
Aloy <AloyOfRivia@protonmail.com>
2023-02-11 11:28:53 -0600

    Adding classes for game board

--- Code/Source/GameBoard.js
+++ Code/Source/GameBoard.js
@@ -1,23 +1,58 @@
 //Handles all the code for the actual game
 
-
 //Player Class
-    //Attributes of a player:
-        //Hand
-        //Number of cards, for easy reference
+class Player {
+    constructor(playerNumber, playerName) {
+
+    }
+
+    getPlayerNumber() {
+
+    }
+
+    getName() {
+
+    }
+
+    getHand() {
+
+    }
+
+    getNumberOfCards() {
+
+    }
+}
 
 
 //Player Hand Class
-    //Should immediately sort after adding or removing
+class PlayerHand{
+    //Does this class need to do any sorting?  Or random order should be fine?
+    constructor(){
+        this.hand = [];
+    }
 
+    addCard(){
+        this.hand.add()
+
+        return trueorfalse;
+    }
+    removeCard(color, number){
+
+    }
+
+    removeCard(){
+    }
+}
 
 //Card Class
 class Card {
-    constructor(color, number) {
-        //Color: Red, Blue, Green, Yellow
+    constructor(color, number, file) {
+        //Color: Red, Blue, Green, Yellow, Wild
         this.color = color;
         //Number: 0-9, Draw, Reverse, Skip
         this.number = number;
+        this.file = file;
+
     }
     getColor(){
         return this.color;
@@ -25,16 +60,28 @@ class Card {
     getNumber(){
         return this.number;
     }
+    getFile(){
+        return this.file;
+    }
 }
 
 
 //Deck Class
-//When a card is discarded, put it at the bottom of the deck.
-//Loop over all of the card files to build the deck
-
-
-//Color: Red, Blue, Green, Yellow
+//108 cards
+    //0: once per color
+    //1-9, draw, reverse, skip: Twice per color
+    //Wild cards: 4 of each type
+    //When a card is discarded, put it at the bottom of the deck.
+    //Loop over all the card files to build the deck
+    //Determine the card type based on the file name
+//Color: Red, Blue, Green, Yellow, Wild
 //Number: 0-9, Draw, Reverse, Skip
+
+//It appaers to be more difficult than I expected to loop over the files in the folder.
+    //Might need to load them in the DOM, hide then, then access them in the DOM when needed
+    //OR: Hardcode all of the file names
+
+
 
 
 
@@ -57,6 +104,14 @@ document.cookie = "UNOGameState=None;expires=" + today.setTime(today.getTime() +
 
 
 //Player Move Function
+// Two Player Rules:
+//
+// For two players, there is a slight change of rules:
+//
+//     Reverse works like Skip
+// Play Skip, and you may immediately play another card
+// If you play a Draw Two or Wild Draw Four card, your opponent has to draw the number of cards required, and then play immediately resumes back on your turn.
+
 
 
 //Current Player Function
